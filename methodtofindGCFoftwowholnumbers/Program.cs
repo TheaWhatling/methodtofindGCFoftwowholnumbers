dd3585633a5d9376e03405deb78bf9e6205ad705
Thea Whatling <K038218@eltham-college.org.uk>
2021-01-11 12:28:45 +0000

    GCF

--- methodtofindGCFoftwowholnumbers/Program.cs
+++ methodtofindGCFoftwowholnumbers/Program.cs
@@ -6,7 +6,36 @@ namespace methodtofindGCFoftwowholnumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            int Num1;
+            int Num2;
+            int Temp1;
+            int Temp2;
+            string Result;
+
+            Console.WriteLine("please enter a whole number");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please enter another whole number");
+            Num2 = Convert.ToInt32(Console.ReadLine());
+
+            Temp1 = Num1;
+            Temp2 = Num2;
+
+            while (Temp1 != Temp2)
+            {
+                if (Temp1 > Temp2)
+                {
+                    Temp1 = Temp1 - Temp2;
+                }
+                else
+                {
+                    Temp2 = Temp2 - Temp1;
+                }
+            }
+
+            Result = Convert.ToString(Temp1);
+  
+            Console.WriteLine(Result, ", is a GCF of ", Num1, " and ", Num2);
         }
     }
 }
